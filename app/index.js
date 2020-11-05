459738863acb778ad8caf72ce29fb121786b6ec9
Dmitry Antipov <antpv99@gmail.com>
2020-11-05 21:32:55 +0200

    Intermediate commit

--- app/index.js
+++ app/index.js
@@ -1,38 +1,19 @@
-// import { Observable } from 'rxjs'
-// import { interval, map } from 'rxjs/operators'
-
-// const stream$ = new Observable(subscriber => {
-//   subscriber.next({
-//     items: []
-//   })
-// }).pipe(
-//   interval(1000, v => v),
-//   map(payload => ({
-//     ...payload,
-//     loading: true
-//   })
-// )
-
-
-// stream$.subscribe(payload => {
-//   console.log(payload)
-// })
-// ***
-
+import anime from 'animejs/lib/anime.es.js'
 import './main.scss'
 
-// Push skeletons
-const wrapper = document.body.querySelector('.async-swipe-wrapper')
-const createSkeleton = value => `<div class="async-swipe-wrapper__skeleton">${value}</div>`
-let renderSkeletonsCount = 30
+class Jonh {
+  constructor(options) {
+    this.rootElement = options.rootElement
+  }
 
-while (renderSkeletonsCount--) {
-  wrapper.innerHTML += createSkeleton(`Skeleton ${renderSkeletonsCount}`)
+  getTest() {
+    console.log(this.rootElement)
+  }
 }
 
-// Code
-console.log('test')
-
-wrapper.addEventListener('touchstart', (e) => {
-  console.log(e)
+const instance = new Jonh({
+  rootElement: document.body.querySelector('.container')
 })
+
+console.log(instance)
+console.log(anime)
